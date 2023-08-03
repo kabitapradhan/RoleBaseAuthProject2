b567bce571c3ed6a3ec55a6d9c777e93dd449ea0
Kabita <pradhankabita7873@gmail.com>
2023-08-03 20:12:50 +0530

    Git security add

--- src/main/java/com/app/entity/User.java
+++ src/main/java/com/app/entity/User.java
@@ -1,7 +1,11 @@
 package com.app.entity;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,7 +23,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,5 +34,35 @@ public class User {
 	private String about;
 	private Date added_date;
 	private String profile_image;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
 }
---
+++ src/main/java/com/app/security/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.app.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.app.entity.User;
+import com.app.repositories.UserRepository;
+
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepo;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// load user from database
+		User user = this.userRepo.findByEmail(username).orElseThrow(()-> new RuntimeException("User Not Found"));
+		return user;
+	}
+
+}
